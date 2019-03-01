3d503d2798b524e00dfb79e051d24031e39175d8
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-02-28 21:16:13 -0300

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,26 @@
+/*
+2 - Faça um programa que leia um número e retorne o fatorial deste número. Crie uma função que encontre
+o resultado. Este programa deverá ser executado quando o usuário apertar 1 no menu da primeira atividade.
+*/
+#include <stdio.h>
+
+int main(void) {
+  int numero, contador, fatorial;
+  
+  printf("\nDigite um numero inteiro maior que zero: ");
+  scanf("%d", &numero);
+  
+  fatorial = numero;
+
+  if(numero > 0){
+    for(contador = 1; numero > contador-1 ;contador++){
+        fatorial = fatorial*(numero-contador);
+      }
+    printf("\n!%d é igual a: %d", numero, fatorial);
+  }
+  else{
+    printf("ERRO.");
+  }
+  
+  return 0;
+}
