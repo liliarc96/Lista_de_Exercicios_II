852cb562b07dfa2f3be6988826f7fb0542ee31b4
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-03-05 21:37:04 -0300

    Update Q8.c

--- Q8.c
+++ Q8.c
@@ -1,5 +1,8 @@
 /*
-8 - Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e exiba na tela a média destas notas. Uma nota válida deve ser, obrigatoriamente, um valor entre 0.0 e 10.0, onde caso a nota não possua um valor válido, este fato deve ser informado ao usuário e o programa termina. Este programa deverá ser executado quando o usuário apertar 7 no menu da primeira atividade.
+8 - Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e exiba na tela a média destas notas.
+Uma nota válida deve ser, obrigatoriamente, um valor entre 0.0 e 10.0, onde caso a nota não possua um valor válido, este 
+fato deve ser informado ao usuário e o programa termina. Este programa deverá ser executado quando o usuário apertar 7 no 
+menu da primeira atividade.
 */
 #include <stdio.h>
 #include <math.h>
