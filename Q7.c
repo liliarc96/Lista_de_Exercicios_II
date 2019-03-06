6b9afa83589eab54d8dd39306536e902d9011c60
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-03-05 21:06:05 -0300

    Update Q7.c

--- Q7.c
+++ Q7.c
@@ -1,5 +1,7 @@
 /*
-7 - Elabore um programa em linguagem C que receba um ano (numérico inteiro) e informe se o ano é bissexto ou não (anos bissextos são múltiplos de 4, portanto, se a divisão do ano por 4 gerar resto igual a zero, o ano é bissexto - use o operador %). Este programa deverá ser executado quando o usuário apertar 6 no menu da primeira atividade.
+7 - Elabore um programa em linguagem C que receba um ano (numérico inteiro) e informe se o ano é bissexto ou não
+(anos bissextos são múltiplos de 4, portanto, se a divisão do ano por 4 gerar resto igual a zero, o ano é bissexto
+- use o operador %). Este programa deverá ser executado quando o usuário apertar 6 no menu da primeira atividade.
 */
 #include <stdio.h>
 #include <math.h>
